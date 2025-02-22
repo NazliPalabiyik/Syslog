cae2f1f96a5221380d2b94721a44f1ebfda0bfa9
NazliPalabiyik <nzlpalabiyik@gmail.com>
2025-02-22 20:37:54 +0300

    syslog.c

--- syslog.c
+++ syslog.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 1024
-
-// Bağlı listedeki her düğümün yapısını tanımlayan Node yapısı
+#define MAX_LINE_LENGTH 1024 // Bağlı listedeki her düğümün yapısını tanımlayan Node yapısı
 typedef struct Node {
     char data[MAX_LINE_LENGTH];
     struct Node* next;
@@ -41,6 +39,18 @@ void display(Node* head) {
     }
 }
 
+// Bağlı listeyi serbest bırakma fonksiyonu
+void free_list(Node* head) {
+    Node* current_node = head;
+    Node* next_node;
+
+    while (current_node != NULL) {
+        next_node = current_node->next;
+        free(current_node);
+        current_node = next_node;
+    }
+}
+
 // Syslog dosyasını okuyan ve verileri bağlı listeye ekleyen fonksiyon
 Node* read_syslog(char* file_path) {
     FILE* file = fopen(file_path, "r");
@@ -48,25 +58,28 @@ Node* read_syslog(char* file_path) {
         perror("Error opening file");
         return NULL;
     }
-    
+
     Node* head = NULL;
     char line[MAX_LINE_LENGTH];
-    
+
     // Dosyadaki her satırı okuyup bağlı listeye ekleme
     while (fgets(line, sizeof(line), file)) {
         append(&head, line);
     }
-    
     fclose(file);
     return head;
 }
 
 int main() {
-    char* file_path = "/var/log/syslog"; // Syslog dosyasının yolu
-    Node* syslog_linked_list = read_syslog(file_path); // Syslog verilerini okuma ve bağlı listeye ekleme
+    // Windows'ta kullanılacak dosya yolu
+    char* file_path = "C:\\path\\to\\your\\file.txt";
+    Node* syslog_linked_list = read_syslog(file_path);
+
+    // Dosya verilerini okuma ve bağlı listeye ekleme
     if (syslog_linked_list != NULL) {
         display(syslog_linked_list); // Bağlı listenin içeriğini yazdırma
+        free_list(syslog_linked_list); // Belleği serbest bırakma
     }
+
     return 0;
 }
-
